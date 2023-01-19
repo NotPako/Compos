cd481f8fe15a3f7f7c656d8777948fa87a890b8c
Pablo Juan <pablo.juan@xing.com>
2023-01-19 20:29:57 +0100

    get started button now redirects to the register page

--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './GetStarted.css';
 import headImage from '../../Resources/FotoLandingPage.jpg';
 import { Button } from 'antd/es/radio';
+import { useNavigate } from 'react-router-dom';
 
 const GetStarted = () => {
+
+    let navigate = useNavigate();
+
     return(
         <div className='parent'>
             <img className='imageHeader' alt= '' src={headImage}>    
             </img>
             <div className='esloganDesign'>The simpliest way to build structures</div>
-            <Button type="primary" shape='round' style={{zIndex:10, position:'absolute', top:'500px', right:'100px'}}>Get started</Button>
+            <Button type="primary" shape='round' style={{zIndex:10, position:'absolute', top:'500px', right:'100px'}} onClick={()=>setTimeout(()=>{navigate("/signup")},250)}>Get started</Button>
             
         </div>
 
